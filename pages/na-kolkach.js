1c967835475db39ee4651cfd3a1d3ed68afd727b
Dzarek <jarekjanas95@gmail.com>
2025-07-07 23:22:56 +0200

    bike page

--- pages/na-kolkach.js
+++ pages/na-kolkach.js
@@ -1,18 +1,18 @@
-import fsPromises from "fs/promises";
-import path from "path";
-// import { cennik } from "../data";
-
 import styled from "styled-components";
 import Head from "next/head";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import { useGlobalContext } from "../components/context";
-import { GiReceiveMoney } from "react-icons/gi";
+// import { MdDirectionsBike, MdOutlineElectricScooter } from "react-icons/md";
+// import { RiEBike2Line } from "react-icons/ri";
 
-const Pricing = ({ data }) => {
-  const { sezonNiski, sezonWysoki } = data;
+const Bikes = () => {
   const { plLanguage } = useGlobalContext();
+
+  const rowerImg = "/images/bikeRent/rower.png";
+  const hulajnogaImg = "/images/bikeRent/hulajnoga.png";
+  const wozekImg = "/images/bikeRent/wozek.png";
 
   useEffect(() => {
     Aos.init({ duration: 1000, disable: "false" });
@@ -65,6 +65,60 @@ const Pricing = ({ data }) => {
             on-site – fast and hassle-free.
           </p>
         )}
+        {plLanguage ? (
+          <h2 className="containerTitle">
+            Może przejażdżka po okolicy na jednym z naszych pojazdów?
+          </h2>
+        ) : (
+          <h2 className="containerTitle">
+            Want to discover the surroundings? <br /> Hop on one of our rental
+            vehicles.
+          </h2>
+        )}
+        <section className="vehicles">
+          <div className="oneVehicle">
+            <h4>{plLanguage ? "rower" : "bike"}</h4>
+            {/* <MdDirectionsBike /> */}
+            <img src={rowerImg} alt="rower" className="bikeImg" />
+            <div className="prices">
+              <p>100.00 PLN/h</p>
+              <p>200.00 PLN/d</p>
+            </div>
+          </div>
+          <div className="oneVehicle">
+            <h4>{plLanguage ? "hulajnoga elektryczna" : "electric scooter"}</h4>
+            {/* <MdOutlineElectricScooter /> */}
+            <img src={hulajnogaImg} alt="hulajnoga" />
+
+            <div className="prices">
+              <p>100.00 PLN/h</p>
+              <p>200.00 PLN/d</p>
+            </div>
+          </div>
+          <div className="oneVehicle">
+            {plLanguage ? (
+              <h4>
+                pojazd elektryczny dla <br /> os. niepełnosprawnych
+              </h4>
+            ) : (
+              <h4>
+                electric vehicle <br /> for disabled people
+              </h4>
+            )}
+            {/* <RiEBike2Line /> */}
+            <img src={wozekImg} alt="wóźek dla os. niepełnosprawnych" />
+
+            <div className="prices">
+              <p>100.00 PLN/h</p>
+              <p>200.00 PLN/d</p>
+            </div>
+          </div>
+        </section>
+        <h3>
+          {plLanguage
+            ? "Zainteresowany? Zadzwoń tel. 793 373 246"
+            : "Interested? Just give us a call at +48 793 373 246"}
+        </h3>
       </Wrapper>
     </>
   );
@@ -143,29 +197,89 @@ const Wrapper = styled.div`
       margin: 6vh auto;
     }
   }
+  .containerTitle {
+    font-family: var(--titleFont);
+    font-size: 2rem;
+    text-transform: uppercase;
+    text-align: center;
+    color: var(--secondaryColor2);
+    letter-spacing: 1px;
+    width: 80%;
+    margin: 0 auto;
+    @media screen and (max-width: 800px) {
+      width: 90%;
+      margin: 0 auto;
+      font-size: 1.4rem;
+    }
+  }
+  .vehicles {
+    width: 70%;
+    margin: 10vh auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    @media screen and (max-width: 800px) {
+      width: 70%;
+      flex-direction: column;
+    }
+    .oneVehicle {
+      overflow: hidden;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 5% 2%;
+      align-items: center;
+      width: 30%;
+      height: 40vh;
+      text-align: center;
+      border-radius: 30px;
+      /* background: #e7e7e7; */
+      background: white;
+      box-shadow: 15px 15px 30px #bebebe, -15px -15px 30px #ffffff;
+      transition: 0.5s;
+      transform: rotate(5deg);
+      @media screen and (max-width: 800px) {
+        width: 100%;
+        height: 50vh;
+        margin-bottom: 5vh;
+        padding: 15% 7%;
+        transform: rotate(2deg);
+      }
+      h4 {
+        text-transform: uppercase;
+        font-size: 1rem;
+        color: var(--secondaryColor2);
+      }
+      /* svg {
+        font-size: 7rem;
+        color: var(--thirdColor);
+      } */
+      img {
+        width: 50%;
+      }
+      .bikeImg {
+        width: 65%;
+      }
+      p {
+        font-weight: 500;
+        font-size: 1.1rem;
+        line-height: 120%;
+      }
+      :hover {
+        transform: rotate(0deg);
+      }
+    }
+  }
+  h3 {
+    margin: 0vh auto 10vh;
+    text-align: center;
+    font-size: 1.6rem;
+    font-weight: 500;
+    width: 80%;
+    @media screen and (max-width: 800px) {
+      font-size: 1.2rem;
+    }
+  }
 `;
 
-export async function getStaticProps() {
-  let data;
-  const filePath = path.join(process.cwd(), "data.json");
-  const jsonData = await fsPromises.readFile(filePath);
-  const localData = JSON.parse(jsonData);
-
-  try {
-    const res = await fetch(
-      "https://sosniegornedata-fee8c-default-rtdb.europe-west1.firebasedatabase.app/price.json"
-    );
-    data = await res.json();
-  } catch (error) {
-    data = localData.price;
-  }
-
-  return {
-    props: {
-      data,
-    },
-    revalidate: 60,
-  };
-}
-
-export default Pricing;
+export default Bikes;
