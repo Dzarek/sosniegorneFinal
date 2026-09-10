97cd964b5e589777b900a1e60c17a6bcae602c90
Dzarek <jarekjanas95@gmail.com>
2026-09-10 12:39:21 +0200

    c

--- pages/_document.js
+++ pages/_document.js
@@ -15,19 +15,8 @@ export default class MyDocument extends Document {
             href="https://fonts.gstatic.com"
             crossOrigin="anonymous"
           />
-
           <link
-            href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500&display=swap"
-            rel="stylesheet"
-          />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700&display=swap"
-            rel="stylesheet"
-          />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Bitter:wght@0,400;0,500;0,600;0,700;0,800&family=Cormorant+Garamond:wght@0,400;0,500;0,600;0,700&display=swap"
             rel="stylesheet"
           />
 
